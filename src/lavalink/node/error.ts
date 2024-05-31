f1ba9072515ee6e4bf8d4ceef3a6f6c781da6631
JustEvil <thatjustevil@gmail.com>
2024-05-31 13:46:23 -0600

    oops

--- src/lavalink/node/error.ts
+++ src/lavalink/node/error.ts
@@ -3,5 +3,5 @@ import { Lavalink } from '../../structures/Lavalink';
 export default new Lavalink({
     name: 'error',
     type: 'node',
-    run: (client, node, error) => client.logger.error(`Music ~ The node: ${node.options.id} has an error: ${JSON.stringify(error, null, 4)}`)
+    run: (client, node, error) => client.logger.error(`The node: ${node.options.id} has an error: ${JSON.stringify(error, null, 4)}`)
 });
--- src/lavalink/node/reconnect.ts
+++ src/lavalink/node/reconnect.ts
@@ -3,5 +3,5 @@ import { Lavalink } from '../../structures/Lavalink';
 export default new Lavalink({
     name: 'reconnecting',
     type: 'node',
-    run: (client, node) => client.logger.warn(`Music ~ The node: ${node.options.id} is reconnecting...`)
+    run: (client, node) => client.logger.warn(`The node: ${node.options.id} is reconnecting...`)
 });
